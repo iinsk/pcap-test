265f6d5cf923108f7206847ca5215512a3e38701
insk <opoet1020@gmail.com>
2024-03-18 10:08:32 -0400

    proto check

--- pcap-test.c
+++ pcap-test.c
@@ -1,6 +1,59 @@
 #include <pcap.h>
 #include <stdbool.h>
 #include <stdio.h>
+#define ETHER_ADDR_LEN 6
+#define ETHERTYPE_IP            0x0800  /* IP protocol */
+
+struct libnet_ethernet_hdr
+{
+    u_int8_t  ether_dhost[ETHER_ADDR_LEN];/* destination ethernet address */
+    u_int8_t  ether_shost[ETHER_ADDR_LEN];/* source ethernet address */
+    u_int16_t ether_type;                 /* protocol */
+};
+
+struct libnet_ipv4_hdr
+{
+#if (LIBNET_LIL_ENDIAN)
+    u_int8_t ip_hl:4,      /* header length */
+        ip_v:4;         /* version */
+#endif
+#if (LIBNET_BIG_ENDIAN)
+    u_int8_t ip_v:4,       /* version */
+        ip_hl:4;        /* header length */
+#endif
+    u_int8_t ip_tos;       /* type of service */
+#ifndef IPTOS_LOWDELAY
+#define IPTOS_LOWDELAY      0x10
+#endif
+#ifndef IPTOS_THROUGHPUT
+#define IPTOS_THROUGHPUT    0x08
+#endif
+#ifndef IPTOS_RELIABILITY
+#define IPTOS_RELIABILITY   0x04
+#endif
+#ifndef IPTOS_LOWCOST
+#define IPTOS_LOWCOST       0x02
+#endif
+    u_int16_t ip_len;         /* total length */
+    u_int16_t ip_id;          /* identification */
+    u_int16_t ip_off;
+#ifndef IP_RF
+#define IP_RF 0x8000        /* reserved fragment flag */
+#endif
+#ifndef IP_DF
+#define IP_DF 0x4000        /* dont fragment flag */
+#endif
+#ifndef IP_MF
+#define IP_MF 0x2000        /* more fragments flag */
+#endif
+#ifndef IP_OFFMASK
+#define IP_OFFMASK 0x1fff   /* mask for fragmenting bits */
+#endif
+    u_int8_t ip_ttl;          /* time to live */
+    u_int8_t ip_p;            /* protocol */
+    u_int16_t ip_sum;         /* checksum */
+    struct in_addr ip_src, ip_dst; /* source and dest address */
+};
 
 void usage() {
     printf("syntax: pcap-test <interface>\n");
@@ -45,6 +98,14 @@ int main(int argc, char* argv[]) {
             break;
         }
         printf("%u bytes captured\n", header->caplen);
+
+        struct libnet_ethernet_hdr *eth_hdr = packet;
+        if(ntohs(eth_hdr->ether_type) != ETHERTYPE_IP) continue;
+        printf("type == %04x \n", ntohs(eth_hdr->ether_type));
+
+        struct libnet_ipv4_hdr *ip_hdr = packet + sizeof(struct libnet_ethernet_hdr); // ipheader initailize
+        if(ip_hdr -> ip_p != IPPROTO_TCP) continue;
+        printf("proto == %04x \n", ip_hdr->ip_p);
     }
 
     pcap_close(pcap);
