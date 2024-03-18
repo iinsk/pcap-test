d51deb0a5c1cfd61d98737a243487eac134a5f21
insk <opoet1020@gmail.com>
2024-03-18 10:27:54 -0400

    Use Magic conch, have to fix

--- pcap-test.c
+++ pcap-test.c
@@ -1,63 +1,19 @@
 #include <pcap.h>
 #include <stdbool.h>
 #include <stdio.h>
-#define ETHER_ADDR_LEN 6
-#define ETHERTYPE_IP            0x0800  /* IP protocol */
+#include <arpa/inet.h>
+#include <netinet/ip.h>
+#include <netinet/tcp.h>
+#include <netinet/if_ether.h>
 
-struct libnet_ethernet_hdr
-{
-    u_int8_t  ether_dhost[ETHER_ADDR_LEN];/* destination ethernet address */
-    u_int8_t  ether_shost[ETHER_ADDR_LEN];/* source ethernet address */
-    u_int16_t ether_type;                 /* protocol */
-};
-
-struct libnet_ipv4_hdr
-{
-#if (LIBNET_LIL_ENDIAN)
-    u_int8_t ip_hl:4,      /* header length */
-        ip_v:4;         /* version */
-#endif
-#if (LIBNET_BIG_ENDIAN)
-    u_int8_t ip_v:4,       /* version */
-        ip_hl:4;        /* header length */
-#endif
-    u_int8_t ip_tos;       /* type of service */
-#ifndef IPTOS_LOWDELAY
-#define IPTOS_LOWDELAY      0x10
-#endif
-#ifndef IPTOS_THROUGHPUT
-#define IPTOS_THROUGHPUT    0x08
-#endif
-#ifndef IPTOS_RELIABILITY
-#define IPTOS_RELIABILITY   0x04
-#endif
-#ifndef IPTOS_LOWCOST
-#define IPTOS_LOWCOST       0x02
-#endif
-    u_int16_t ip_len;         /* total length */
-    u_int16_t ip_id;          /* identification */
-    u_int16_t ip_off;
-#ifndef IP_RF
-#define IP_RF 0x8000        /* reserved fragment flag */
-#endif
-#ifndef IP_DF
-#define IP_DF 0x4000        /* dont fragment flag */
-#endif
-#ifndef IP_MF
-#define IP_MF 0x2000        /* more fragments flag */
-#endif
-#ifndef IP_OFFMASK
-#define IP_OFFMASK 0x1fff   /* mask for fragmenting bits */
-#endif
-    u_int8_t ip_ttl;          /* time to live */
-    u_int8_t ip_p;            /* protocol */
-    u_int16_t ip_sum;         /* checksum */
-    struct in_addr ip_src, ip_dst; /* source and dest address */
-};
+#define ETHER_HEADER_LEN 14
+#define MAX_BYTES_TO_CAPTURE 10
 
 void usage() {
     printf("syntax: pcap-test <interface>\n");
-    printf("sample: pcap-test wlan0\n");
+    printf("sample: pcap-test eth0\n");
+    // by unknown problem, can't find&use wlan0
+    // capture eto0 packet
 }
 
 typedef struct {
@@ -77,9 +33,54 @@ bool parse(Param* param, int argc, char* argv[]) {
     return true;
 }
 
+void print_hex(const u_char *data, int size) {
+    int bytes_to_print = size > MAX_BYTES_TO_CAPTURE ? MAX_BYTES_TO_CAPTURE : size;
+    for (int i = 0; i < bytes_to_print; i++) {
+        printf("%02x ", data[i]);
+    }
+    printf("\n");
+}
+
+void packet_handler(u_char *user, const struct pcap_pkthdr *pkt_header, const u_char *packet) {
+    struct ethhdr *eth_header = (struct ethhdr *)packet;
+    if (ntohs(eth_header->h_proto) != ETHERTYPE_IP) return; // Only IP packets
+
+    struct iphdr *ip_header = (struct iphdr *)(packet + ETHER_HEADER_LEN);
+    if (ip_header->protocol != IPPROTO_TCP) return; // Only TCP packets
+
+    struct tcphdr *tcp_header = (struct tcphdr *)((u_char *)ip_header + ip_header->ihl * 4);
+
+    printf("Source MAC: ");
+    for (int i = 0; i < ETH_ALEN; i++) {
+        printf("%02x", eth_header->h_source[i]);
+        if (i < ETH_ALEN - 1) printf(":");
+    }
+
+    printf("\nDestination MAC: ");
+    for (int i = 0; i < ETH_ALEN; i++) {
+        printf("%02x", eth_header->h_dest[i]);
+        if (i < ETH_ALEN - 1) printf(":");
+    }
+
+    printf("\nSource IP: %s\n", inet_ntoa(*(struct in_addr *)&ip_header->saddr));
+    printf("Destination IP: %s\n", inet_ntoa(*(struct in_addr *)&ip_header->daddr));
+
+    printf("Source Port: %u\n", ntohs(tcp_header->source));
+    printf("Destination Port: %u\n", ntohs(tcp_header->dest));
+
+    int data_offset = ETHER_HEADER_LEN + ip_header->ihl * 4 + tcp_header->doff * 4;
+    int data_size = pkt_header->caplen - data_offset;
+    if (data_size > 0) {
+        printf("Payload (Hex): ");
+        print_hex(packet + data_offset, data_size);
+    } else {
+        printf("No Payload\n");
+    }
+    printf("-----------------------------------------------\n");
+}
+
 int main(int argc, char* argv[]) {
-    if (!parse(&param, argc, argv))
-        return -1;
+    if (!parse(&param, argc, argv)) return -1;
 
     char errbuf[PCAP_ERRBUF_SIZE];
     pcap_t* pcap = pcap_open_live(param.dev_, BUFSIZ, 1, 1000, errbuf);
@@ -97,16 +98,9 @@ int main(int argc, char* argv[]) {
             printf("pcap_next_ex return %d(%s)\n", res, pcap_geterr(pcap));
             break;
         }
-        printf("%u bytes captured\n", header->caplen);
-
-        struct libnet_ethernet_hdr *eth_hdr = packet;
-        if(ntohs(eth_hdr->ether_type) != ETHERTYPE_IP) continue;
-        printf("type == %04x \n", ntohs(eth_hdr->ether_type));
-
-        struct libnet_ipv4_hdr *ip_hdr = packet + sizeof(struct libnet_ethernet_hdr); // ipheader initailize
-        if(ip_hdr -> ip_p != IPPROTO_TCP) continue;
-        printf("proto == %04x \n", ip_hdr->ip_p);
+        packet_handler(NULL, header, packet);
     }
 
     pcap_close(pcap);
+    return 0;
 }
